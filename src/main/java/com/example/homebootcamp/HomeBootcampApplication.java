1c697d56a6663a48275af3671f18c0ca48f7092d
SlintPunk <artur.matuszkiewicz@gmail.com>
2019-08-01 20:15:37 +0200

    Spring Boot App Init

---
+++ src/main/java/com/example/homebootcamp/HomeBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.example.homebootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeBootcampApplication.class, args);
+	}
+
+}
